8e8afc61d100bd74c25e3f97d3de001113a02d60
JSilva <jsilvaisyourbro@gmail.com>
2024-08-17 16:46:08 -0500

    Updated Functionality
    Changed the functionality to not have a weird potential slowdown if you let go of space when you're already falling down.

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -4,22 +4,25 @@ extends CharacterBody2D
 const SPEED = 800.0
 const JUMP_VELOCITY = -1000.0
 var LetSpaceGo = false
+var ReachedApex = false
 func _physics_process(delta: float) -> void:
 	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta * 2
-		
+	elif is_on_floor():
+		LetSpaceGo = false
+		ReachedApex = false
 	
 	# Handle jump.
 	if Input.is_action_just_pressed("Player_Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		LetSpaceGo = false
 	
 	# Handle Jump Height
-	if Input.is_action_just_released("Player_Jump") and !LetSpaceGo:
+	if Input.is_action_just_released("Player_Jump") and not LetSpaceGo and velocity.y <= 0:
 		velocity.y = 0 #get_real_velocity().y * 0.25;
 		LetSpaceGo = true
+		ReachedApex = true
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
