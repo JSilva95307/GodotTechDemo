34cb1c04f0cde341266f02d42d17620a6e7e71d7
JSilva <jsilvaisyourbro@gmail.com>
2024-08-17 15:56:31 -0500

    Edited Inputs
    I found the input system and added an input for space specifically, I also messed with the jump ability so that you now have the ability to control how high you can jump with holding the space bar.

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,19 +1,26 @@
 extends CharacterBody2D
 
 
-const SPEED = 1000.0
-const JUMP_VELOCITY = -800.0
-
-
+const SPEED = 800.0
+const JUMP_VELOCITY = -1000.0
+var LetSpaceGo = false
 func _physics_process(delta: float) -> void:
+	
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
+		velocity += get_gravity() * delta * 2
+		
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Player_Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		LetSpaceGo = false
+	
+	# Handle Jump Height
+	if Input.is_action_just_released("Player_Jump") and !LetSpaceGo:
+		velocity.y = 0 #get_real_velocity().y * 0.25;
+		LetSpaceGo = true
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
